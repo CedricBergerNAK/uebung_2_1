f2b27478fce83ad4f13054c226d9cd0a00b9e5c2
15427 <15427@A001-12.nordakademie.intern>
2017-10-26 10:58:22 +0200

    update: quadratische Wurzel eingefügt.

--- src/uebung_2_1/PqFormel.java
+++ src/uebung_2_1/PqFormel.java
@@ -3,14 +3,14 @@ package uebung_2_1;
 public class PqFormel {
 
 	public static void main(String[] args) {
-		double p=5;
-		double q=4;
+		double p=8;
+		double q=14;
 		
 		double unterderwurzel = ((p/2)*(p/2)-q);
 		double vorderwurzel = (-1*(p/2));
 
-		double x1 = vorderwurzel+unterderwurzel;
-		double x2 = vorderwurzel-unterderwurzel;
+		double x1 = vorderwurzel+Math.sqrt(unterderwurzel);
+		double x2 = vorderwurzel-Math.sqrt(unterderwurzel);
 		
 		System.out.println("x1 = "+ x1);
 		System.out.println("x2 = "+ x2);
